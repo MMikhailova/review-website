a406ecb18c7e9012d9c63ea239757ccea12b8867
MMikhailova <naplesaries2014@gmail.com>
2024-01-17 10:02:31 +0100

    index changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,13 +32,12 @@ const allowedOrigins=process.env.NODE_ENV==="production"?prodOrigins:devOrigin
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
-      console.log(true);
+
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
-  }, credentials: true,
-  methods:['GET','POST','PUT','DELETE']
+  }, credentials: true
 }))
 
 

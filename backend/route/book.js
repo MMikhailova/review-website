8abc74e46a0142f94bebee526269016b354e04c4
MMikhailova <naplesaries2014@gmail.com>
2024-01-17 21:59:38 +0100

    remove verifyToken

--- backend/route/book.js
+++ backend/route/book.js
@@ -7,7 +7,7 @@ const router = express.Router()
 router.post('/books/add-book', bookControllers.addBook)
 router.put("/books/add-review/:id",verifyToken, bookControllers.addReview);
 router.get("/books/get-books", bookControllers.getBooks);
-router.get("/books/get-book/:id",verifyToken, bookControllers.getBook);
+router.get("/books/get-book/:id", bookControllers.getBook);
 router.put("/books/update-book/:id", bookControllers.updateBook);
 router.get("/books/get-reviews/:id", verifyToken,bookControllers.getBookReviews);
 export default router;

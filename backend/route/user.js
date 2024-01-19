ab300539198169eb804ee1c64c5972beaa76e0a2
MMikhailova <naplesaries2014@gmail.com>
2024-01-19 14:40:11 +0100

    cookie final

--- backend/route/user.js
+++
@@ -1,8 +0,0 @@
-import express  from "express";
-import userControllers from "../controllers/userControllers.js";
-import verifyToken from "../middleware/verifyToken.js";
-
-const router = express.Router();
-router.put('/user/add-favorites',verifyToken, userControllers.addToFavorites)
-router.put("/user/remove-favorites",verifyToken, userControllers.deleteFromFavorites);
-export default router;
--- frontend/src/utils/checkMatch.js
+++
@@ -1,6 +0,0 @@
-
-const matchPasswords = (password1, password2) => {
-    return password1 === password2;
-};
-
-export default matchPasswords;
